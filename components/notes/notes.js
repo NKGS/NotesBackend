da9d46332bbdc3c2dbf0cd48a3ba43aadf1a97a7
Nikita Kothari <Kothari.Nikita@mahindra.com>
2020-11-12 22:07:38 +0530

    code for notes save,update,get,delete

--- components/notes/notes.js
+++ components/notes/notes.js
@@ -5,17 +5,17 @@ const { successFormat } = require("../../helper/formatResponse");
 class Notes {
     async getAllNotes(data) {
         return new Promise((resolve, reject) => {
-            try{
-                const notesData = NotesModel.find({userId: data}, function(err, result) {
-                    console.log('result - ',result)
-                    if(err) 
+            try {
+                const notesData = NotesModel.find({ userId: data }, function (err, result) {
+                    console.log('result - ', result)
+                    if (err)
                         reject(err)
                     else
-                        resolve(result)  
-                    
+                        resolve(result)
+
                 })
-                
-            } catch(e) {
+
+            } catch (e) {
                 console.log(`errror - ${error}`)
                 reject(e)
             }
@@ -24,35 +24,71 @@ class Notes {
 
     async postNote(data) {
         try {
-            const notesData = await NotesModel.findOne({userId: data.userId}, function (err, val) {
+            const notesData = await NotesModel.findOne({ userId: data.userId }, function (err, val) {
                 if (err) {
                     console.log(err)
                     console.log('nikitaa')
                 }
-              })
+            })
 
-              console.log('notesData - ',notesData)
-              //!_.isEmpty(userData)
-              if (!notesData) {
-                  const Notes = new NotesModel(data)
-                  await Notes.save(data)
-                  console.log('Notes does not exist add new')
-                  return
-              } 
-        } catch(e) {
+            if (!notesData) {
+                console.log('Notes does not exist add new')
+                const Notes = new NotesModel(data)
+                await Notes.save(data)
+                return
+            } else {
+                //const data = data.notes;
+                let q = data.task._id ? { "userId": data.userId, "taskList._id": data.task._id } : { "userId": data.userId }
+                
+                if(data.task._id) //means update
+                    await NotesModel.updateOne(q, { $set: { "taskList.$": data.task } }, { safe:true })
+                else //means insert
+                    await NotesModel.updateOne(q, { $push:  {"taskList": data.task } }, { upsert: true, safe: true })
+                console.log('Notes updated ',)
+
+                return
+            }
+        } catch (e) {
             console.log(`errror - ${e}`)
+        }
+    }
+
+    async deleteNotes(data) {
+        try {
+            const notesData = await NotesModel.findOne({ userId: data.userId }, function (err, val) {
+                if (err) {
+                    console.log(err)
+                    console.log('nikitaa')
+                }
+            })
+
+            if (notesData) {
+                if(data.type == "all") {
+                    let q = { "userId": data.userId }
+                    await NotesModel.updateOne(q, { $pop: { "userId": data.userId } }, { safe:true })
+                    return 
+                } else {
+                    let q = { "userId": data.userId }
+                    console.log(q)
+                    await NotesModel.updateOne(q, { $pull: { "taskList": { _id: data.task._id } } }, { safe:true })
+                    console.log("Deleted successfully")
+                    return
+                }
+            }
+            return "No such record exists!!"
+        } catch(e) {
+            console.log('error ',e)
         }
     }
 }
 
-const notes = async(req,res) => {
+const notes = async (req, res) => {
     try {
         const notesCls = new Notes();
         const notes = await notesCls.getAllNotes(req.user.id)
-        console.log('notes - ',notes)
-        
+        console.log('notes - ', notes)
         res.status(200).send(successFormat(`success`, `Data fetched successfully`, notes, []))
-    } catch(e) {
+    } catch (e) {
         console.log(`error - ${e}`)
         const code = (Object.prototype.hasOwnProperty.call(e, 'status') ? e.code : 500)
         const response = errorResponse(e)
@@ -60,21 +96,35 @@ const notes = async(req,res) => {
     }
 }
 
-const saveNotes = async(req,res) => {
+const saveNotes = async (req, res) => {
     try {
         const notesCls = new Notes();
+        const data = req.body
         const notes = notesCls.postNote({
             userId: req.user.id,
-            taskList: [
-                {
-                task: "One",
-                taskEndDate: "22 Oct,2020",
-                taskPriority: 1,
-                }
-            ]
+            task: data
         })
-        console.log('notes - ',notes)
+        console.log('notes - ', notes)
         res.status(200).send(successFormat(`success`, `Data saved successfully`, notes, []))
+    } catch (e) {
+        console.log(`error - ${e}`)
+        const code = (Object.prototype.hasOwnProperty.call(e, 'status') ? e.code : 500)
+        const response = errorResponse(e)
+        res.status(code).send(response)
+    }
+}
+
+const deleteNote = async (req, res) => {
+    try {
+        const notesCls = new Notes();
+        const data = req.body
+        console.log('data - ', data)
+        const notes = notesCls.deleteNotes({
+            userId: req.user.id,
+            task: data
+        })
+        console.log('notes - ', notes)
+        res.status(200).send(successFormat(`success`, `Data deleted successfully`, notes, []))
     } catch(e) {
         console.log(`error - ${e}`)
         const code = (Object.prototype.hasOwnProperty.call(e, 'status') ? e.code : 500)
@@ -83,4 +133,4 @@ const saveNotes = async(req,res) => {
     }
 }
 
-module.exports = {notes, saveNotes}
+module.exports = { notes, saveNotes, deleteNote }
--- routing/notesRoute.js
+++ routing/notesRoute.js
@@ -2,8 +2,9 @@ const express = require('express')
 const router = express.Router()
 
 const middleware = require('../middleware')
-const { notes, saveNotes } = require('../components/notes/notes')
+const { notes, saveNotes, deleteNote } = require('../components/notes/notes')
 
 router.get("/", middleware, notes)
 router.post("/", middleware, saveNotes)
+router.delete("/", middleware, deleteNote)
 module.exports = router
